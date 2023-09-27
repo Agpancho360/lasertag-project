423c836bd2c87865f01ee086e2ba42b31cd95b5e
Your Name <you@example.com>
2023-09-26 23:57:26 -0500

    Left off here. Just need to add the logo image

--- splash.py
+++ splash.py
@@ -5,15 +5,15 @@ splash_root.title("Splash Screen")
 splash_root.geometry("700x700")
 
 # Load the image and keep a reference to it
-img = PhotoImage(file="logo.jpg")
-splash_label = Label(splash_root, image=img, font=("Helvetica", 18))
+# img = PhotoImage(file="logo.jpg")
+splash_label = Label(splash_root, text= "Add image here", font=("Helvetica", 18))
 splash_label.pack(pady=20)
 
 def main_window():
     splash_root.destroy()
     root = Tk()
     root.title("Main Window")
-    root.geometry("200x200")
+    root.geometry("700x700")
 
 # Call the main_window function after 3000 milliseconds (3 seconds)
 splash_root.after(3000, main_window)
