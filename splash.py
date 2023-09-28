9e288540f23074b02d7285651343133538d06428
Xanjt17 <alexander.tavarez249@gmail.com>
2023-09-28 14:19:32 -0500

    Added logo, need to adjust size

--- splash.py
+++ splash.py
@@ -1,19 +1,23 @@
 from tkinter import *
 
+
 splash_root = Tk()
 splash_root.title("Splash Screen")
 splash_root.geometry("700x700")
 
+
 # Load the image and keep a reference to it
-# img = PhotoImage(file="logo.jpg")
-splash_label = Label(splash_root, text= "Add image here", font=("Helvetica", 18))
+img = PhotoImage(file="logo.png") # Replace "image.png" with the actual path to your image
+splash_label = Label(splash_root, image=img)
 splash_label.pack(pady=20)
+
 
 def main_window():
     splash_root.destroy()
     root = Tk()
     root.title("Main Window")
     root.geometry("700x700")
+
 
 # Call the main_window function after 3000 milliseconds (3 seconds)
 splash_root.after(3000, main_window)
