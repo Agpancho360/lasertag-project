55a3aa85be1b0e10d47be1ea32e945dc69d2a695
Xanjt17 <alexander.tavarez249@gmail.com>
2023-09-28 14:34:08 -0500

    Changed background image size

--- splash.py
+++ splash.py
@@ -1,24 +1,25 @@
-from tkinter import *
-
-
-splash_root = Tk()
-splash_root.title("Splash Screen")
-splash_root.geometry("700x700")
-
-
-# Load the image and keep a reference to it
-img = PhotoImage(file="logo.png") # Replace "image.png" with the actual path to your image
-splash_label = Label(splash_root, image=img)
-splash_label.pack(pady=20)
-
+import tkinter as tk
+from tkinter import PhotoImage
 
 def main_window():
     splash_root.destroy()
-    root = Tk()
+    root = tk.Tk()
     root.title("Main Window")
     root.geometry("700x700")
 
+# Create the splash screen window
+splash_root = tk.Tk()
+splash_root.title("Splash Screen")
+splash_root.geometry("700x700")
+
+# Load the image and resize it to fit the 700x700 window
+img = PhotoImage(file="logo.png")
+img = img.subsample(img.width() // 700, img.height() // 700)
+
+splash_label = tk.Label(splash_root, image=img)
+splash_label.pack()
 
 # Call the main_window function after 3000 milliseconds (3 seconds)
 splash_root.after(3000, main_window)
+
 splash_root.mainloop()
