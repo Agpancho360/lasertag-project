7d4b44ba933d623e9c71441d5783b036cc09b5cc
Xanjt17 <alexander.tavarez249@gmail.com>
2023-09-30 15:34:25 -0500

    Updated front end design

--- splash.py
+++ splash.py
@@ -3,14 +3,14 @@ from tkinter import ttk
 
 def main_window():
     splash_root.withdraw()
-    root = tk.Toplevel()
+    root = tk.Tk()
     root.title("Laser Tag Game")
-    root.geometry("800x800")
+    root.geometry("1400x600")  # Adjusted width to accommodate both tables
     root.configure(bg="black")
 
-    def create_table(parent, row_offset, team_name, background_color, num_empty_rows=10):
+    def create_table(parent, team_name, background_color, num_empty_rows=10):
         table_frame = tk.Frame(parent, bg=background_color)
-        table_frame.grid(row=row_offset - 1, column=0, columnspan=4, pady=(10, 5), sticky="w")
+        table_frame.pack(side="left", padx=10)
 
         team_label = tk.Label(table_frame, text=team_name, font=("Impact", 20, "bold"), bg=background_color, fg="white")
         team_label.grid(row=0, column=0, columnspan=4, pady=(10, 5))
@@ -68,41 +68,67 @@ def main_window():
         ("2", "Alex", "Guzman", "Thunder_Lips"),
     ]
 
-    red_table_frame = create_table(root, row_offset=1, team_name="Red Team", background_color="#FF0000")
-    blue_table_frame = create_table(root, row_offset=6, team_name="Blue Team", background_color="#0000FF")
+    # Create the left frame for input elements
+    left_frame = tk.Frame(root, bg="black")
+    left_frame.pack(side="left", padx=10)
 
-    input_frame = tk.Frame(root, bg="black")
-    input_frame.grid(row=14, column=0, columnspan=4, pady=(10, 5))
+    # Create a frame for the player entry section with a white background
+    player_entry_frame = tk.Frame(left_frame, bg="white", padx=10, pady=10)
+    player_entry_frame.grid(row=0, column=0, columnspan=4, pady=(10, 5))
 
-    id_label = tk.Label(input_frame, text="ID:", font=("Helvetica", 12), bg="black", fg="white")
-    id_label.grid(row=0, column=0, padx=10)
-    id_entry = tk.Entry(input_frame, font=("Helvetica", 12))
-    id_entry.grid(row=0, column=1, padx=10)
+    # Create a Label for the "Player Entry" title
+    player_entry_label = tk.Label(player_entry_frame, text="Player Entry", font=("Helvetica", 16, "bold"), bg="white", fg="black")
+    player_entry_label.grid(row=0, column=0, columnspan=4, pady=(10, 5))
 
-    first_name_label = tk.Label(input_frame, text="First Name:", font=("Helvetica", 12), bg="black", fg="white")
-    first_name_label.grid(row=0, column=2, padx=10)
-    first_name_entry = tk.Entry(input_frame, font=("Helvetica", 12))
-    first_name_entry.grid(row=0, column=3, padx=10)
+    id_label = tk.Label(player_entry_frame, text="ID:", font=("Helvetica", 12), bg="white", fg="black")
+    id_label.grid(row=1, column=0, padx=10)
+    id_entry = tk.Entry(player_entry_frame, font=("Helvetica", 12))
+    id_entry.grid(row=1, column=1, padx=10)
 
-    last_name_label = tk.Label(input_frame, text="Last Name:", font=("Helvetica", 12), bg="black", fg="white")
-    last_name_label.grid(row=1, column=0, padx=10)
-    last_name_entry = tk.Entry(input_frame, font=("Helvetica", 12))
-    last_name_entry.grid(row=1, column=1, padx=10)
+    first_name_label = tk.Label(player_entry_frame, text="First Name:", font=("Helvetica", 12), bg="white", fg="black")
+    first_name_label.grid(row=1, column=2, padx=10)
+    first_name_entry = tk.Entry(player_entry_frame, font=("Helvetica", 12))
+    first_name_entry.grid(row=1, column=3, padx=10)
 
-    codename_label = tk.Label(input_frame, text="Codename:", font=("Helvetica", 12), bg="black", fg="white")
-    codename_label.grid(row=1, column=2, padx=10)
-    codename_entry = tk.Entry(input_frame, font=("Helvetica", 12))
-    codename_entry.grid(row=1, column=3, padx=10)
+    last_name_label = tk.Label(player_entry_frame, text="Last Name:", font=("Helvetica", 12), bg="white", fg="black")
+    last_name_label.grid(row=2, column=0, padx=10)
+    last_name_entry = tk.Entry(player_entry_frame, font=("Helvetica", 12))
+    last_name_entry.grid(row=2, column=1, padx=10)
+
+    codename_label = tk.Label(player_entry_frame, text="Codename:", font=("Helvetica", 12), bg="white", fg="black")
+    codename_label.grid(row=2, column=2, padx=10)
+    codename_entry = tk.Entry(player_entry_frame, font=("Helvetica", 12))
+    codename_entry.grid(row=2, column=3, padx=10)
 
     team_var = tk.StringVar()
     team_var.set("Red Team")
-    team_label = tk.Label(input_frame, text="Select Team:", font=("Helvetica", 12), bg="black", fg="white")
-    team_label.grid(row=2, column=0, padx=10)
-    team_menu = tk.OptionMenu(input_frame, team_var, "Red Team", "Blue Team")
-    team_menu.grid(row=2, column=1, padx=10)
+    team_label = tk.Label(player_entry_frame, text="Select Team:", font=("Helvetica", 12), bg="white", fg="black")
+    team_label.grid(row=3, column=0, padx=10)
+    team_menu = tk.OptionMenu(player_entry_frame, team_var, "Red Team", "Blue Team")
+    team_menu.grid(row=3, column=1, padx=10)
 
-    add_button = tk.Button(input_frame, text="Add Player", font=("Helvetica", 12), command=add_player)
-    add_button.grid(row=2, column=2, padx=10, pady=10)
+    add_button = tk.Button(player_entry_frame, text="Add Player", font=("Helvetica", 12), command=add_player)
+    add_button.grid(row=3, column=2, padx=10, pady=10)
+
+    # Create a label to display the selected team
+    selected_team_label = tk.Label(left_frame, text="Selected Team: Red Team", font=("Helvetica", 12), bg="black", fg="white")
+    selected_team_label.grid(row=4, column=0, columnspan=4, padx=10)
+
+    def update_selected_team_label(*args):
+        selected_team_label.config(text="Selected Team: " + team_var.get())
+
+    # Monitor changes to the team_var and call the update_selected_team_label function
+    team_var.trace_add("write", update_selected_team_label)
+
+    # Create the right frame for tables
+    right_frame = tk.Frame(root, bg="black")
+    right_frame.pack(side="right", padx=10)
+
+    # Create the red table on the left side
+    red_table_frame = create_table(right_frame, team_name="Red Team", background_color="#FF0000")
+    
+    # Create the blue table on the right side
+    blue_table_frame = create_table(right_frame, team_name="Blue Team", background_color="#0000FF")
 
     # Initialize tables with default data
     update_table(red_table_frame, red_data)
