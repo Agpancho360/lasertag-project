b1d342d4291d4f98a1b820d3833c3c38545adc88
AGPancho03 <agpancho03@gmail.com>
2023-10-03 00:43:28 -0500

    changed design (DONT TOUCH FOR NOW)

--- sample.py
+++ sample.py
@@ -15,20 +15,27 @@ def main_window():
     root = tkinter.Tk()
     root.title("Laser Tag Game")
     root.geometry("1400x600")
+    root.configure(bg="black")
     root.grid_rowconfigure(0, weight=1)
     root.grid_columnconfigure(0, weight=1)
 
-    def create_table(parent, team_name, column):
-        table_frame = customtkinter.CTkFrame(parent)
-        table_frame.grid(row=0, column=column, padx= 40)
+    def create_table(parent, team_name, column, background_color, border_color):
+        table_frame = customtkinter.CTkFrame(
+            parent, fg_color=background_color, border_width=5, border_color=border_color)
+        table_frame.grid(row=0, column=column, padx=40, pady=40)
         # Create labels for table headers
-        team_label = customtkinter.CTkLabel(table_frame, text=team_name)
-        id_label = customtkinter.CTkLabel(table_frame, text="ID" )
-        first_name_label = customtkinter.CTkLabel(table_frame, text="First Name")
-        last_name_label = customtkinter.CTkLabel(table_frame, text="Last Name")
-        codename_label = customtkinter.CTkLabel(table_frame, text="Codename")
-        #places labels to team table
-        team_label.grid(row=0, column=0, columnspan=4)
+        team_label = customtkinter.CTkLabel(
+            table_frame, text=team_name, font=("Impact", 25), text_color="whitesmoke")
+        id_label = customtkinter.CTkLabel(
+            table_frame, text="ID", font=("Impact", 15), text_color="whitesmoke")
+        first_name_label = customtkinter.CTkLabel(
+            table_frame, text="First Name", font=("Impact", 15), text_color="whitesmoke")
+        last_name_label = customtkinter.CTkLabel(
+            table_frame, text="Last Name", font=("Impact", 15), text_color="whitesmoke")
+        codename_label = customtkinter.CTkLabel(
+            table_frame, text="Codename", font=("Impact", 15), text_color="whitesmoke")
+        # places labels to team table
+        team_label.grid(row=0, column=0, columnspan=4, pady=15)
         id_label.grid(row=1, column=0, padx=25, pady=15)
         first_name_label.grid(row=1, column=1, padx=25, pady=15)
         last_name_label.grid(row=1, column=2, padx=25, pady=15)
@@ -43,13 +50,16 @@ def main_window():
 
         return table_frame
     # Function to update the player data table
+
     def update_table(table_frame, data):
         for i, data_row in enumerate(data, start=2):
             for j, cell_data in enumerate(data_row):
-                label = customtkinter.CTkLabel(table_frame, text=cell_data)
+                label = customtkinter.CTkLabel(table_frame, text=cell_data, font=(
+                    "Arial", 10), text_color="whitesmoke")
                 label.grid(row=i, column=j)
+
     def add_player():
-        team = "Red"#team_var.get()  # Get the selected team
+        team = team_entry.get()
         first_name = first_name_entry.get()
         last_name = last_name_entry.get()
         codename = codename_entry.get()
@@ -68,32 +78,37 @@ def main_window():
             update_table(blue_table_frame, blue_team_data)
 
         # Clear the input fields
-        #id_entry.delete(0, customtkinter.CTkEntry.END)
+        # id_entry.delete(0, customtkinter.CTkEntry.END)
         first_name_entry.delete(0, customtkinter.CTkEntry.END)
         last_name_entry.delete(0, customtkinter.CTkEntry.END)
         codename_entry.delete(0, customtkinter.CTkEntry.END)
    # **************************************Left Frame *******************************************************
-    left_frame = customtkinter.CTkFrame(root)
+    left_frame = customtkinter.CTkFrame(root, fg_color="transparent")
     left_frame.grid(row=0, column=0)
     # **************************************Player Entry Screen *******************************************************
-    player_entry_frame = customtkinter.CTkFrame(left_frame)
+    player_entry_frame = customtkinter.CTkFrame(left_frame,fg_color="#8d99ae", border_width=5, border_color="#2b2d42")
     player_entry_frame.grid(row=0, column=0)
     # creates labels, entry, and button for player entry screen
     player_entry_label = customtkinter.CTkLabel(
-        player_entry_frame, text="Player Entry")
+        player_entry_frame, text="Player Entry", font=("Impact", 25), text_color="whitesmoke")
     first_name_label = customtkinter.CTkLabel(
-        player_entry_frame, text="First Name:")
+        player_entry_frame, text="First Name:", font=("Impact", 15), text_color="whitesmoke")
     first_name_entry = customtkinter.CTkEntry(
         player_entry_frame, placeholder_text="First Name")
     last_name_label = customtkinter.CTkLabel(
-        player_entry_frame, text="Last Name:")
+        player_entry_frame, text="Last Name:", font=("Impact", 15), text_color="whitesmoke")
     last_name_entry = customtkinter.CTkEntry(
         player_entry_frame, placeholder_text="Last Name")
     codename_label = customtkinter.CTkLabel(
-        player_entry_frame, text="Codename:")
+        player_entry_frame, text="Codename:", font=("Impact", 15), text_color="whitesmoke")
     codename_entry = customtkinter.CTkEntry(
         player_entry_frame, placeholder_text="Codename")
-    submitButton = customtkinter.CTkButton(player_entry_frame, text="Submit", command=add_player)
+    team_label = customtkinter.CTkLabel(
+        player_entry_frame, text="Select Team:", font=("Impact", 15), text_color="whitesmoke")
+    team_entry = customtkinter.CTkOptionMenu(
+        player_entry_frame, values=["Blue", "Red"])
+    submitButton = customtkinter.CTkButton(
+        player_entry_frame, text="Submit", corner_radius=  25, command=add_player)
     # places items onto player entry screen
     player_entry_label.grid(row=0, column=0, columnspan=2, padx=25, pady=15)
     first_name_label.grid(row=1, column=0, padx=25, pady=15)
@@ -102,17 +117,22 @@ def main_window():
     last_name_entry.grid(row=2, column=1, padx=25, pady=15)
     codename_label.grid(row=3, column=0, padx=25, pady=15)
     codename_entry.grid(row=3, column=1, padx=25, pady=15)
-    submitButton.grid(row=4, column=0, columnspan=2, padx=25, pady=15)
+    team_label.grid(row=4, column=0, padx=25, pady=15)
+    team_entry.grid(row=4, column=1, padx=25, pady=15)
+    submitButton.grid(row=5, column=0, columnspan=2, padx=25, pady=15)
 
-     # **************************************Right frame *******************************************************
-    right_frame = customtkinter.CTkFrame(root,fg_color="transparent")
+    # **************************************Right frame *******************************************************
+    right_frame = customtkinter.CTkFrame(root, fg_color="transparent")
     right_frame.grid(row=0, column=1)
-     # **************************************Red Table Frame *******************************************************
-    red_table_frame = create_table(right_frame, "Red Team", 0)
+    # **************************************Red Table Frame *******************************************************
+    red_table_frame = create_table(
+        right_frame, "Red Team", 0, "#e23b4a", "#900A22")
     # **************************************Blue Table Frame *******************************************************
-    blue_table_frame = create_table(right_frame, "Blue Team",1 )
+    blue_table_frame = create_table(
+        right_frame, "Blue Team", 1, "#61bbe7", "#0577ac")
     update_table(red_table_frame, red_team_data)
     update_table(blue_table_frame, blue_team_data)
+
 
 # Create the splash screen
 splash_root = tkinter.Tk()
